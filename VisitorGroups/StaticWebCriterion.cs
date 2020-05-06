79e5d82b7e9a9fe70dc4729758b781b5d516a8a3
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-05-06 21:38:04 +0200

    Solves issue #13

---
+++ VisitorGroups/StaticWebCriterion.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Personalization.VisitorGroups;
+using StaticWebEpiserverPlugin.Channels;
+using System.Security.Principal;
+using System.Web;
+
+namespace StaticWebEpiserverPlugin.VisitorGroups
+{
+    [VisitorGroupCriterion(
+       Category = "Technical",
+       DisplayName = "StaticWeb User",
+       Description = "Checks if currently visited by StaticWebEpiServerPlugin for page caching")]
+    public class StaticWebCriterion : CriterionBase<StaticWebCriterionSettings>
+    {
+        public override bool IsMatch(IPrincipal principal, HttpContextBase httpContext)
+        {
+            return new StaticWebChannel().IsActive(httpContext);
+        }
+    }
+}
---
+++ VisitorGroups/StaticWebCriterionSettings.cs
@@ -0,0 +1,12 @@
+﻿using EPiServer.Personalization.VisitorGroups;
+
+namespace StaticWebEpiserverPlugin.VisitorGroups
+{
+    public class StaticWebCriterionSettings : CriterionModelBase
+    {
+        public override ICriterionModel Copy()
+        {
+            return ShallowCopy();
+        }
+    }
+}
